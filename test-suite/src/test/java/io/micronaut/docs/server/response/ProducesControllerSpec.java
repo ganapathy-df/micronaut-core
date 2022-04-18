73f2cba4e8be53777349ef683ac055b137b0b073
Ganapathy <ganapathy.df@gmail.com>
2022-04-18 19:35:13 +0530

    cap-issue-id-98621344-105

--- test-suite/src/test/java/io/micronaut/docs/server/response/ProducesControllerSpec.java
+++ test-suite/src/test/java/io/micronaut/docs/server/response/ProducesControllerSpec.java
@@ -15,6 +15,7 @@
  */
 package io.micronaut.docs.server.response;
 
+import io.micronaut.docs.ExtractedSeparateClass21111;
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
@@ -37,9 +38,7 @@ public class ProducesControllerSpec {
     @BeforeClass
     public static void setupServer() {
         server = ApplicationContext.run(EmbeddedServer.class, Collections.singletonMap("spec.name", "producesspec"));
-        client = server
-                .getApplicationContext()
-                .createBean(HttpClient.class, server.getURL());
+        client = ExtractedSeparateClass21111.getClient11688(server);
     }
 
     @AfterClass
--- test-suite/src/test/java/io/micronaut/docs/server/response/StatusControllerSpec.java
+++ test-suite/src/test/java/io/micronaut/docs/server/response/StatusControllerSpec.java
@@ -15,6 +15,7 @@
  */
 package io.micronaut.docs.server.response;
 
+import io.micronaut.docs.ExtractedSeparateClass21111;
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
@@ -39,9 +40,7 @@ public class StatusControllerSpec {
     @BeforeClass
     public static void setupServer() {
         server = ApplicationContext.run(EmbeddedServer.class, Collections.singletonMap("spec.name", "httpstatus"));
-        client = server
-                .getApplicationContext()
-                .createBean(HttpClient.class, server.getURL());
+        client = ExtractedSeparateClass21111.getClient11688(server);
     }
 
     @AfterClass
